412365bbbbca8cd899fb3c691852b2920e807b2e
darmankulova <imetoffa1@gmail.com>
2023-07-11 15:07:02 +0600

    projeckt 2

---
+++ script.js
@@ -0,0 +1,60 @@
+// let a = 'hello'
+// let b = 'Ivan'
+// let c = a[0].toUpperCase()
+// let d =a.slice(1)
+// let e =b[0].toLowerCase()
+// let f =b.slice(1)
+// console.log(c+d+' '+e+f)
+
+// let a = 'lorem ipsum dolor sit amet'
+// let b = a.length
+// console.log(b)
+
+// let a ='children'
+// let b =a.slice(0,7).toUpperCase()
+// let c =a.slice(-1).toLowerCase()
+// console.log(b+c)
+
+// let a = 'boss'
+// let b = a.slice(2)
+// console.log(a[0]+b+a[1])
+
+// let a ='sunday'
+// console.log(a[3])
+
+
+// let a ='user'
+// let b ='new'
+// console.log(b+' '+a.slice(0,2))
+
+// let a ='monday'
+// let b ='day'
+// console.log(b+b+a.slice(0,3))
+
+// let a ='HELLO'
+// console.log(a[0].toLowerCase()+a[1]+a[2].toLowerCase()+a[3]+a[4].toLowerCase())
+
+
+// let a='monkey'
+// let b='banana'
+// console.log(a[0]+b.slice(1,4)+a[3])
+
+// let a=5
+// let b='400'
+// let c=b.replace(0,a)
+// console.log(c)
+
+
+// let a='hello'
+// let b=a[2].replace('l','k')
+// let c=a[3].replace('l','s')
+// console.log(a.slice(0,2)+b+c+a.slice(-1))
+
+let a='hello'
+let b=a[2].split('l').join('k')
+let c=a[3].split('l').join('s')
+console.log(a.slice(0,2)+b+c+a.slice(-1))
+
+
+
+
